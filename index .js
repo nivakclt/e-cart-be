b8de02568293745f7f9c7de10a5468bedf949381
nivakclt <nivakclt@gmail.com>
2026-08-16 20:01:41 +0530

    fon

---
+++ index .js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+
+const router = jsonServer.router("db.json");
+
+const middleware = jsonServer.defaults();
+
+const PORT = 3000;
+
+server.use(middleware);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log("E-cart Server Running!!");
+});
